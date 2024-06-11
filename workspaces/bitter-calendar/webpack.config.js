c375b0cc8f6675cbebea5d374f91b96e7d2a0194
Nayeong Lee <98475086+yienaa@users.noreply.github.com>
2024-06-12 02:08:42 +0900

    Create bitter-calendar workspace (feat. 한땀한땀)

---
+++ workspaces/bitter-calendar/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './index.tsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      },
+    ],
+  },
+};
